ae8d727943185b3196b92025dea8a5b373693650
Eric Ryu <eryuu.g@gmail.com>
2017-07-11 21:07:33 -0400

    added handleclick to Topbar to call for updateCurrentPage in App

--- src/Topbar/Topbar.js
+++ src/Topbar/Topbar.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import CONSTANTS from '../Constants.js';
 
 class Topbar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this._handleClick.bind(this);
+    }
+
+    _handleClick(pageName) {
+        this.props.updateCurrentPage(pageName);
+    }
+
     render() {
+        var tabStyle = {
+            width : '15%',
+            height : '80%',
+            float : 'left',
+            textAlign : 'center'
+        };
+
         return (
             <div>
+                <div
+                    style={tabStyle}
+                    onClick={() => this._handleClick(CONSTANTS.PAGES.LOGIN)}
+                >{CONSTANTS.PAGES.LOGIN}</div>
+                <div
+                    style={tabStyle}
+                    onClick={() => this._handleClick(CONSTANTS.PAGES.HOME)}
+                >{CONSTANTS.PAGES.HOME}</div>
+                <div
+                    style={tabStyle}
+                    onClick={() => this._handleClick(CONSTANTS.PAGES.INVENTORY)}
+                >{CONSTANTS.PAGES.INVENTORY}</div>
+                <div
+                    style={tabStyle}
+                    onClick={() => this._handleClick(CONSTANTS.PAGES.HISTORY)}
+                >{CONSTANTS.PAGES.HISTORY}</div>
             </div>
         );
     }
