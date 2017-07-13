e14d9a272e9f5335f34bcba0730d7fd02e7d522b
Eric Ryu <eryuu.g@gmail.com>
2017-07-13 01:54:39 -0400

    created Database.js and added getInstance and authenticate function

---
+++ src/Database/Database.js
@@ -0,0 +1,23 @@
+let instance = null;
+
+class Database {
+
+    static getInstance() {
+        if (!Database.instance) {
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+
+    authenticate(name, password) {
+        // query database for name and password match.
+
+        // hardcoded result
+        let correctName = (name === "david" || name === "eric");
+        let correctPassword = (password === "david" || password === "eric");
+        return (correctName && correctPassword);
+    }
+
+}
+
+export default Database
