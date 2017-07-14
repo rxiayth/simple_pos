17eda357a7c8ada5aa91408ae62b62786f4c27ba
Eric Ryu <eryuu.g@gmail.com>
2017-07-13 20:14:47 -0400

    fixed minor unused references

--- src/Main/Login.js
+++ src/Main/Login.js
@@ -18,8 +18,6 @@ class Login extends Component {
     }
 
     render() {
-        let isLoggedIn = this.props.isLoggedIn;
-
         return (
             <div>
                 {this.isLoggedIn()}
