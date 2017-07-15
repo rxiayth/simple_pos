34bac929ecb5f9be8df2ee1a109468853d5bee3a
Eric Ryu <eryuu.g@gmail.com>
2017-07-15 01:50:46 -0400

    updated Home to render either Checkout or ProductSearch and Cart

--- src/Common/Button.js
+++ src/Common/Button.js
@@ -11,7 +11,7 @@ class Button extends Component {
             </div>
         );
     }// render
-}
+}// Button
 
 Button.propTypes = {
     style : PropTypes.shape({
--- src/Main/Home.js
+++ src/Main/Home.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
-import ProductSearch from './ProductSearch'
-import Cart from './Cart.js'
-import Checkout from './Checkout.js'
+
+import ProductSearch from './ProductSearch';
+import Cart from './Cart.js';
+import Checkout from './Checkout.js';
+import CONSTANTS from '../Constants.js';
+
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentPage : CONSTANTS.PAGES.PRODUCTSEARCH
+        }
+
+        this.updateCurrentPage = this.updateCurrentPage.bind(this);
+    }
+
+    updateCurrentPage(pageName) {
+        this.setState({currentPage : pageName});
+    }// updateCurrentPage
+
 
     render() {
         return (
             <div>
-                <ProductSearch />
-                <Cart />
+                {this._renderCurrentPage()}
             </div>
         );
-    }
-}
+    }// render
+
+    _renderCurrentPage() {
+        let result = <div></div>;
+        switch (this.state.currentPage) {
+            case (CONSTANTS.PAGES.PRODUCTSEARCH) : {
+                result = (
+                    <div>
+                        <ProductSearch />
+                        <Cart
+                            goToCheckOut={() =>
+                                this.updateCurrentPage(CONSTANTS.PAGES.CHECKOUT)
+                            }
+                        />
+                    </div>
+                );
+                break;
+            };
+            case (CONSTANTS.PAGES.CHECKOUT) : {
+                result = (
+                    <div>
+                        <Checkout />
+                    </div>
+                );
+                break;
+            }
+        }// switch
+        return result;
+    }// _renderCurrentPage
+}// Home
 
 export default Home;
